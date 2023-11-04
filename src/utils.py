2c8d2bf1fdd873d386e198415b45170f140fadb4
Alexandr Zubarev <zualvi@yandex.ru>
2023-11-04 07:14:54 +0300

    Added create_database function in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
+import psycopg2
 import requests
+
+from config import config
 
 
 def get_employers_and_vacancies(data: list) -> list:
@@ -15,3 +18,23 @@ def get_employers_and_vacancies(data: list) -> list:
         })
 
     return result
+
+
+def create_database(name: str, params: dict) -> None:
+    """Функция создает базу двнных.
+    Если база данных с таким названием уже существует,
+    то удаляет ее.
+    Принимает на вход название базы данных и параметры для подключения."""
+    conn = psycopg2.connect(database='postgres', **params)
+    conn.autocommit = True
+
+    with conn.cursor() as cur:
+        cur.execute('select * from pg_database')
+        all_databases = cur.fetchall()
+        for item in all_databases:
+            if item[1] == name:
+                cur.execute(f'DROP DATABASE {name}')
+                break
+        cur.execute(f'CREATE DATABASE {name}')
+
+    conn.close()
