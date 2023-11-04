e61d95a1a5830e34740f09ef346d1abb1bcfa9e4
Alexandr Zubarev <zualvi@yandex.ru>
2023-11-04 12:31:50 +0300

    Created fill_database and format_salary functions in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,6 @@
 import psycopg2
 import requests
 
-from config import config
-
 
 def get_data(data: list) -> list:
     """Функция принимает на вход список ID компаний с HH.RU.
@@ -21,7 +19,7 @@ def get_data(data: list) -> list:
 
 
 def create_database(db_name: str, params: dict) -> None:
-    """Функция создает базу двнных.
+    """Функция создает базу данных.
     Если база данных с таким названием уже существует,
     то удаляет ее.
     Принимает на вход название базы данных и параметры для подключения."""
@@ -49,17 +47,60 @@ def create_tables(db_name: str, params: dict) -> None:
 
     with conn.cursor() as cur:
         cur.execute('CREATE TABLE companies('
-                       'company_id serial PRIMARY KEY,'
-                       'company_name varchar(50) NOT NULL,'
-                       )
+                    'company_id serial PRIMARY KEY,'
+                    'company_name varchar(50) NOT NULL,'
+                    'link varchar(200) NOT NULL)')
 
         cur.execute('CREATE TABLE vacancies('
-                       'vacancy_id serial PRIMARY KEY,'
-                       'company_id int REFERENCES companies (company_id) NOT NULL,'
-                       'title_vacancy varchar(150) NOT NULL,'
-                       'salary int,'
-                       'link varchar(200) NOT NULL,'
-                       'description text,')
+                    'vacancy_id serial PRIMARY KEY,'
+                    'company_id int REFERENCES companies (company_id) NOT NULL,'
+                    'title_vacancy varchar(200) NOT NULL,'
+                    'salary int,'
+                    'link varchar(200) NOT NULL,'
+                    'description text)')
 
     conn.commit()
     conn.close()
+
+
+def format_salary(value):
+    if value is not None:
+        if value['from'] is not None and value['to'] is not None:
+            return round((value['from'] + value['to']) / 2)
+        elif value['from'] is not None:
+            return value['from']
+        elif value['to'] is not None:
+            return value['to']
+
+
+def fill_database(data: list, db_name: str, params: dict) -> None:
+    """
+    Функция для заполнения таблиц данными.
+    Принимает в качестве аргументов список словарей,
+    имя базы данных и параметры для подключения
+    """
+    conn = psycopg2.connect(database=db_name, **params)
+
+    with conn.cursor() as cur:
+        for item in data:
+            cur.execute('INSERT INTO companies (company_name, link)'
+                        'VALUES (%s, %s)'
+                        'returning company_id',
+                        (item['employer'].get("name"),
+                         item['employer'].get("alternate_url")))
+
+            company_id = cur.fetchone()[0]
+
+            for part in item['vacancies']:
+                salary = format_salary(part["salary"])
+                cur.execute('INSERT INTO vacancies'
+                            '(company_id, title_vacancy, salary, link, description)'
+                            'VALUES (%s, %s, %s, %s, %s)',
+                            (company_id,
+                             part["name"],
+                             salary,
+                             part["alternate_url"],
+                             part["snippet"].get("responsibility")))
+    conn.commit()
+    conn.close()
+
